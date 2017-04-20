26cd6cba565550c59323f20393fdbef3cba856d7
Alexander <Lynx@Alexanders-MacBook-Pro.local>
2017-04-20 10:55:11 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('./../sass/style.scss');
+
+ReactDOM.render(
+    <h2>Parking init</h2>,
+    document.getElementById('parking')
+);
